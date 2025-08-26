ca8820a86364cca33e8de7faee306564d81351d2
Marcelo Burgos <marchelo.bm@gmail.com>
2025-08-26 03:15:55 -0400

    v2.0 cambios
    cambios para nuevo android

--- Inventario/app/src/main/java/com/example/marchelo/inventario/MainActivity.java
+++ Inventario/app/src/main/java/com/example/marchelo/inventario/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.marchelo.inventario;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
--- Inventario/app/src/main/java/com/example/marchelo/inventario/ingresarDatos.java
+++ Inventario/app/src/main/java/com/example/marchelo/inventario/ingresarDatos.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
